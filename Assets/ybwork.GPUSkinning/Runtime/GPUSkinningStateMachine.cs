faaebc5e02cdca20b5ca44986e2c8f81c59492b7
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-07-11 14:12:49 +0800

    尝试重新播放一个循环动作，且当前动作就是目标动作，则跳过

---
+++ Assets/ybwork.GPUSkinning/Runtime/GPUSkinningStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+public class GPUSkinningStateMachine
+{
+    private readonly Dictionary<int, bool> _isLoops = new();
+    private readonly Dictionary<int, int> _nextStates = new();
+
+    public void RegisterOnceState(int state, int nextState)
+    {
+        _nextStates[state] = nextState;
+    }
+
+    public void RegisterLoopState(int state)
+    {
+        _isLoops[state] = true;
+    }
+
+    internal bool TryGetNextState(int state, out int nextState)
+    {
+        return _nextStates.TryGetValue(state, out nextState);
+    }
+
+    internal bool GetStateIsLoop(int state)
+    {
+        if (!_isLoops.TryGetValue(state, out bool isLoop))
+            isLoop = false;
+        return isLoop;
+    }
+}
