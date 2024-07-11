1edc9111b7cec223afb50917eaa2ed2da98b8fc1
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-07-11 14:13:30 +0800

    增加示例

---
+++ Assets/ybwork.GPUSkinning/Samples~/ybwork.GPUSkinning.Sample/TestGPU.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using ybwork.Async;
+
+public class TestGPU : MonoBehaviour
+{
+    [SerializeField] GameObject _prefab;
+
+    void Start()
+    {
+        Transform parent = new GameObject("Roles").transform;
+        for (int i = 0; i < 50; i++)
+        {
+            for (int j = 0; j < 60; j++)
+            {
+                GameObject go = Instantiate(_prefab, parent);
+                go.transform.position = new Vector3(i, 0, j);
+                GPUSkinningComponent gpuSkinningComponent = go.AddComponent<GPUSkinningComponent>();
+                YueTask.Delay(Random.value * 100).Then(() =>
+                {
+                    gpuSkinningComponent.SwitchState(Random.Range(0, 4));
+                });
+            }
+        }
+    }
+}
