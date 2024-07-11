72c2dcf7327f67d54d7f01a3b0037b3e92e32c60
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-07-11 14:13:30 +0800

    增加示例

---
+++ Samples~/ybwork.GPUSkinning.Sample/TestUnit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(GPUSkinningComponent))]
+public class TestUnit : MonoBehaviour
+{
+    GPUSkinningComponent _skinningComponent;
+
+    private void Awake()
+    {
+        _skinningComponent = GetComponent<GPUSkinningComponent>();
+        _skinningComponent.StateMachine.RegisterLoopState(0);
+        _skinningComponent.StateMachine.RegisterOnceState(1, 0);
+        _skinningComponent.StateMachine.RegisterLoopState(2);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad0 + i))
+                _skinningComponent.SwitchState(i);
+        }
+    }
+}
