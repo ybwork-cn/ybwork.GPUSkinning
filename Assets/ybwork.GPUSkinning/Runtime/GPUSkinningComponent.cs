d141f443c2352a4609a2d01ebb9124097c9cc231
zhangtao <18131773370@163.com>
2024-08-21 19:10:09 +0800

    优化Update性能

--- Assets/ybwork.GPUSkinning/Runtime/GPUSkinningComponent.cs
+++ Assets/ybwork.GPUSkinning/Runtime/GPUSkinningComponent.cs
@@ -1,74 +1,13 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.Events;
-
-[Serializable]
-internal class GPUSkinningData
-{
-    public bool ForceUpdate;
-    public bool Loop;
-    public int AnimIndex;
-    public float CurrentTime;
-
-    public bool LastAnimLoop;
-    public int LastAnimIndex;
-    public float LastAnimExitTime;
-
-    private readonly Renderer _renderer;
-    private readonly MaterialPropertyBlock _propBlock = new();
-
-    public GPUSkinningData(Renderer renderer)
-    {
-        _renderer = renderer;
-        _renderer.GetPropertyBlock(_propBlock);
-    }
-
-    public void SwitchState(int state, bool loop)
-    {
-        LastAnimLoop = Loop;
-        LastAnimIndex = AnimIndex;
-        LastAnimExitTime = CurrentTime;
-        Loop = loop;
-        AnimIndex = state;
-        CurrentTime = 0;
-
-        _propBlock.SetFloat("_Loop", Loop ? 1 : 0);
-        _propBlock.SetFloat("_AnimIndex", AnimIndex);
-        _propBlock.SetFloat("_CurrentTime", CurrentTime);
-
-        _propBlock.SetFloat("_LastAnimLoop", LastAnimLoop ? 1 : 0);
-        _propBlock.SetFloat("_LastAnimIndex", LastAnimIndex);
-        _propBlock.SetFloat("_LastAnimExitTime", LastAnimExitTime);
-
-        _renderer.SetPropertyBlock(_propBlock);
-    }
-
-    public void Update(float deltaTime)
-    {
-        if (Application.isEditor && ForceUpdate)
-        {
-            _propBlock.SetFloat("_Loop", Loop ? 1 : 0);
-            _propBlock.SetFloat("_AnimIndex", AnimIndex);
-
-            _propBlock.SetFloat("_LastAnimLoop", LastAnimLoop ? 1 : 0);
-            _propBlock.SetFloat("_LastAnimIndex", LastAnimIndex);
-            _propBlock.SetFloat("_LastAnimExitTime", LastAnimExitTime);
-        }
-
-        CurrentTime += deltaTime;
-        _propBlock.SetFloat("_CurrentTime", CurrentTime);
-        _renderer.SetPropertyBlock(_propBlock);
-    }
-}
 
 public class GPUSkinningComponent : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
     [SerializeField] GPUSkinningData _gpuSkinningData;
     public GPUSkinningStateMachine StateMachine { get; private set; }
+    public readonly Event<int> OnStateSwitched = new Event<int>();
     private GPUSkinningInfo _gpuSkinningInfo;
-    private readonly UnityEvent<int> _onStateSwitched = new();
-    public UnityEvent<int> OnStateSwitched => _onStateSwitched;
     private bool _canCallback = true;
 
     private void Awake()
@@ -88,7 +27,7 @@ public class GPUSkinningComponent : MonoBehaviour
         if (!_gpuSkinningData.Loop && _gpuSkinningInfo.AnimaitonLengths[animIndex] <= _gpuSkinningData.CurrentTime)
         {
             if (_canCallback)
-                _onStateSwitched.Invoke(animIndex);
+                OnStateSwitched.Invoke(animIndex);
             _canCallback = false;
 
             if (StateMachine.TryGetNextState(animIndex, out int nextStateIndex))
