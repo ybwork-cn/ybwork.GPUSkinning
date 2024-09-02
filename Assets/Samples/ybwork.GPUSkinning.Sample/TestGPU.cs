c9c10c996960984c42eb0a07de2d3ca512ff6307
zhangtao <18131773370@163.com>
2024-09-02 15:23:25 +0800

    优化测试代码

--- Assets/Samples/ybwork.GPUSkinning.Sample/TestGPU.cs
+++ Assets/Samples/ybwork.GPUSkinning.Sample/TestGPU.cs
@@ -15,7 +15,7 @@ public class TestGPU : MonoBehaviour
                 GameObject go = Instantiate(_prefab, parent);
                 go.transform.position = new Vector3(i, 0, j);
                 GPUSkinningComponent gpuSkinningComponent = go.AddComponent<GPUSkinningComponent>();
-                YueTask.Delay(Random.value * 100).Then(() =>
+                YueTask.Delay(Random.value * 10).Then(() =>
                 {
                     gpuSkinningComponent.SwitchState(Random.Range(0, 4));
                 });
