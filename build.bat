ee74f61f934ff8a2f56f7ac7bb8a3b266630e3bb
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-12-15 14:49:21 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,9 +3,7 @@ SET ToolName=upm
 ::设置模块源路径
 SET ToolAssetPath=Assets/ybwork.GPUSkinning
 
-git branch -D %ToolName%
-git push origin -d %ToolName%
-::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
-git subtree split -P %ToolAssetPath% --branch %ToolName%
+::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容，并推送到origin仓库
+git subtree push -P %ToolAssetPath% origin %ToolName%
 
 pause
